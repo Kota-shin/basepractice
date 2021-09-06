0e52c30da18160e8cce5ff4f3fe934bd035aed87
KotaGit <Kota7arai@gmail.com>
2021-09-06 23:17:43 +0900

    basepractice
    basepractice

---
+++ Assets/Scripts/BallCtrl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCtrl : MonoBehaviour
+{
+    [Header("初速の速さ")] public float speed;
+
+    void Start()
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody>();
+
+        Vector3 force = new Vector3(10.0f, 10.0f, 0.0f) * speed;
+
+        rb.AddForce(force);
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
