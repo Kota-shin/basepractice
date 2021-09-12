1d48f93912290353991be61e7569befc7b765027
KotaGit <Kota7arai@gmail.com>
2021-09-12 11:07:45 +0900

    Block塗り前＿完成
    Block塗り前＿完成

--- Assets/Scripts/GameCtrl.cs
+++ Assets/Scripts/GameCtrl.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameCtrl : MonoBehaviour
 {
     public GameObject ClearText;
     public GameObject GameOverText;
+
+    public GameObject RetryButton;
 
     void Start()
     {
@@ -21,11 +24,25 @@ public class GameCtrl : MonoBehaviour
         if (count == 0)
         {
             ClearText.SetActive(true);
+            //クリアしたらBallを消滅
+            GameObject obj = GameObject.Find("Ball");
+            Destroy(obj);
         }
 
-        if (count1 == 0)
+        //Ballがなくなりかつ、Blockが残っているときにゲームオーバー
+        if (count1 == 0 && count != 0)
         {
+            //ゲームオーバー、リトライボタン表示
             GameOverText.SetActive(true);
+            RetryButton.SetActive(true);
         }
+    }
+
+    public void PushRetryButton()
+    {
+        //現在のScene名を取得する
+        Scene loadScene = SceneManager.GetActiveScene();
+        //Sceneの読み直し
+        SceneManager.LoadScene(loadScene.name);
     }
 }
